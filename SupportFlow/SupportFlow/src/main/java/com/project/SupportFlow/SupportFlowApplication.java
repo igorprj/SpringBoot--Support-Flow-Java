fe91f738b45a082787fa3f7af09a0b1536072721
igorcarvalho <cigor0089@gmail.com>
2026-09-02 12:28:39 -0300

    chore: initialize project

---
+++ SupportFlow/SupportFlow/src/main/java/com/project/SupportFlow/SupportFlowApplication.java
@@ -0,0 +1,13 @@
+package com.project.SupportFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportFlowApplication.class, args);
+	}
+
+}
