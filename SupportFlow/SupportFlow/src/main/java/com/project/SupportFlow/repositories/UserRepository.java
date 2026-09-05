9736af11afc6396b624d5e3dd07350183e6157bf
igorcarvalho <cigor0089@gmail.com>
2026-09-04 22:41:14 -0300

    feat: add UserRepository

---
+++ SupportFlow/SupportFlow/src/main/java/com/project/SupportFlow/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.project.SupportFlow.repositories;
+
+import com.project.SupportFlow.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
