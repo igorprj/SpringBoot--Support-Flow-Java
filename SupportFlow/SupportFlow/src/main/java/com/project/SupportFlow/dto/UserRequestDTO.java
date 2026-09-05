529d5dd3050892c6cce1a82d949a55008dc9344d
igorcarvalho <cigor0089@gmail.com>
2026-09-04 22:44:55 -0300

    feat: add UserDTOs and UserService

---
+++ SupportFlow/SupportFlow/src/main/java/com/project/SupportFlow/dto/UserRequestDTO.java
@@ -0,0 +1,8 @@
+package com.project.SupportFlow.dto;
+
+public record UserRequestDTO(
+        String name,
+        String email,
+        String password
+) {
+}
---
+++ SupportFlow/SupportFlow/src/main/java/com/project/SupportFlow/service/UserService.java
@@ -0,0 +1,4 @@
+package com.project.SupportFlow.service;
+
+public class UserService {
+}
