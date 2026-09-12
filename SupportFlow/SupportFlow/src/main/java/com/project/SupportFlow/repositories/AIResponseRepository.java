f2a3aec5fef6245fcfe903d54515920b063f1729
igorcarvalho <cigor0089@gmail.com>
2026-09-12 15:56:33 -0300

    feat: create AI repository

---
+++ SupportFlow/SupportFlow/src/main/java/com/project/SupportFlow/repositories/AIResponseRepository.java
@@ -0,0 +1,7 @@
+package com.project.SupportFlow.repositories;
+
+import com.project.SupportFlow.model.AIResponse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AIResponseRepository extends JpaRepository<AIResponse, Integer> {
+}
